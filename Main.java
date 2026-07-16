cd49d28bed6910a39ec723de41001d6f7125c3f1
Sourav <souravsep282005@gmail.com>
2026-07-16 19:29:08 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static  void main(String[] args){
+        int a=10;
+        int b=20;
+      //  System.out.println("The sum of a and b is (a+b): " + (a+b));]
+        int mul=a*b;
+        System.out.println("The multiplication of a and b is (a*b): " + mul);
+    }
+}
