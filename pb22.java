2fabc87cda876caeb37c9a4401079da19c5bcfec
Sourav <souravsep282005@gmail.com>
2026-07-23 19:23:54 +0530

    Update pb22.java

--- pb22.java
+++ pb22.java
@@ -1,16 +1,16 @@
 class Solution {
     public int addDigits(int num) {
         
-        while (num >= 10) { // Repeat until num becomes a single digit
+        while (num >= 10) { 
             
             int sum = 0;
             
             while (num > 0) {
-                sum += num % 10; // Get last digit and add it
-                num /= 10;       // Remove last digit
+                sum += num % 10; 
+                num /= 10;       
             }
             
-            num = sum; // Store the sum back in num
+            num = sum; 
         }
         
         return num;
